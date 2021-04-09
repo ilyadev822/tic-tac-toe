7c8846f2a273a5e757ad244738a3ed22edcdca19
ilyadev822 <ilya3333@gmail.com>
2021-04-09 07:58:04 +0300

    tic-tac-toe project

--- tic-tac-toe/start.js
+++ tic-tac-toe/start.js
@@ -12,18 +12,4 @@ function clickMe(id) {
         HumanMove(row, cell);
     }
 }
-var test = ["x", 1, "x", "y", "y", 5, 6, 7, 8];
-prompt("test array : \n " + printArr());
-function printArr() {
-    var myStr = "|";
-    for (var i = 0; i < Game.BoardArr.length; i++) {
-        for (var j = 0; j < Game.BoardArr[i].length; j++) {
-            myStr += Game.BoardArr[i][j] + "|";
-        }
-        myStr += " \n";
-        myStr += " --------- ";
-        myStr += " \n";
-    }
-    return myStr;
-}
 //# sourceMappingURL=start.js.map
